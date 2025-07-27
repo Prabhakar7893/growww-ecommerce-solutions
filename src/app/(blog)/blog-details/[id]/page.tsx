83180ac0ca82fab86e06ac1d6a2d5add1c554508
Prabhakar7893 <prabhakar7729@gmail.com>
2025-07-27 20:33:19 +0530

    changed file like blog2

--- src/app/(blog)/blog-details/[id]/page.tsx
+++ src/app/(blog)/blog-details/[id]/page.tsx
@@ -1,21 +1,16 @@
-// src/app/(blog)/blog-details/[id]/page.tsx
-
+import React from "react";
 import { Metadata } from "next";
-import { blog_data } from "@/data/blog-data";
 import BlogDetailsMain from "@/pages/blog/blog-details";
-import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Growww - Blog Details page",
 };
 
-// Correct typing for App Router dynamic route page
-export default function BlogDetailsPage({ params }: { params: { id: string } }) {
-  const blog = blog_data.find((blog) => blog.id === Number(params.id));
+const BlogDetailsPage = () => {
+  return (
+    <BlogDetailsMain/>
+  );
+};
 
-  if (!blog) {
-    notFound(); // uses Next.js built-in 404
-  }
+export default BlogDetailsPage;
 
-  return <BlogDetailsMain blog={blog} />;
-}
