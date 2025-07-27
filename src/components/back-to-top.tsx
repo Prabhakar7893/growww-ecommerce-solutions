1b6a86274da7df9daeb18e1f9522560dbb0007dc
Prabhakar7893 <prabhakar7729@gmail.com>
2025-07-27 20:52:47 +0530

    fix: remove duplicate React import in back-to-top.tsx

--- src/components/back-to-top.tsx
+++ src/components/back-to-top.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { CSSProperties } from 'react';
+import { CSSProperties } from 'react';
 import whatsapp_png from '@/assets/img/home-04/footer/whatsapp.png';
 
 
