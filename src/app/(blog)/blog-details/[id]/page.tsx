d45a93d3c3f620f52c7ffcbd9780502e6ba30290
Prabhakar7893 <prabhakar7729@gmail.com>
2025-07-27 20:08:24 +0530

    changed blog details file

--- src/app/(blog)/blog-details/[id]/page.tsx
+++ src/app/(blog)/blog-details/[id]/page.tsx
@@ -1,30 +1,23 @@
+// src/app/(blog)/blog-details/[id]/page.tsx
+
 import { Metadata } from "next";
 import { blog_data } from "@/data/blog-data";
-import BlogDetailsMain from "@/components/blog/blog-details"; 
+import BlogDetailsMain from "@/pages/blog/blog-details";
 
+// Dynamic metadata export
 export const metadata: Metadata = {
   title: "Growww - Blog Details page",
 };
 
-type BlogDetailsPageProps = {
-  params: {
-    id: string;
-  };
-};
+// This is how Next.js App Router expects props
+export default function BlogDetailsPage({ params }: { params: { id: string } }) {
+  const blog = blog_data.find((blog) => blog.id === Number(params.id));
 
-export default function BlogDetailsPage({ params }: BlogDetailsPageProps) {
-  const { id } = params;
-  const blogId = Number(params.id); // convert string to number
-  const blog = blog_data.find((blog) => blog.id === blogId);
-
-  if (!blog) {
-    return (
-      <div className="text-center pt-24">
-        <h2>Blog not found</h2>
-        <p>ID: {params.id}</p>
-      </div>
-    );
-  }
-
-  return <BlogDetailsMain blog={blog} />;
+  return blog ? (
+    <BlogDetailsMain blog={blog} />
+  ) : (
+    <div className="text-center pt-100">
+      Blog not found with id: {params.id}
+    </div>
+  );
 }
